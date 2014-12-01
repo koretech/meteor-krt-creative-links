b392d3d147632a8c62e232fccef4cb95fb41f876
Mike Kornelson <mike@koretech.ca>
2014-12-01 10:25:06 -0600

    Upgraded to Meteor 1.0

--- package.js
+++ package.js
@@ -3,13 +3,13 @@ var client = 'client', server = 'server', both = ['client', 'server'];
 Package.describe({
 	name: 'krt:creative-links',
 	summary: 'Creative Links Package',
-	version: '0.1.0',
+	version: '0.1.1',
 	git: 'https://github.com/koretech/meteor-krt-creative-links.git'
 });
 
 Package.onUse(function(api){
 
-	api.versionsFrom('METEOR@0.9.4');
+	api.versionsFrom('METEOR@1.0');
 
 	api.addFiles([
 		'lib/creative-links.css'
